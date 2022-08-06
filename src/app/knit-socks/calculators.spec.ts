4b3dab2f0a760d5a9a0328d369f6e423d9e79196
Eveline Visee <eveline.visee@gmail.com>
2022-08-06 13:48:58 +0200

    Add test case for sock size 43

--- src/app/knit-socks/calculators.spec.ts
+++ src/app/knit-socks/calculators.spec.ts
@@ -8,7 +8,7 @@ const gaugeSixPly = new GaugeSettings({horizontalDistance: defaultDistanceInCM, 
 const footE = new FootMeasurements({ legCircumference: 23, instepHeight: 6, forefootCircumference: 22, length: 24});
 const footA = new FootMeasurements({ legCircumference: 24, instepHeight: 5.5, forefootCircumference: 24.5, length: 25}); // unsure length
 const footM = new FootMeasurements({ length: 27, legCircumference: 24, instepHeight: 5.2, forefootCircumference: 24.4});
-const footB = new FootMeasurements({ length: 26.5, legCircumference: 24, instepHeight: 6, forefootCircumference: 26}); // unsure length
+const footB = new FootMeasurements({ length: 26, legCircumference: 26, instepHeight: 6, forefootCircumference: 26}); // unsure length
 
 describe('getStitchesInRound', ()=>{
   it('size EU38', ()=>{
@@ -59,6 +59,23 @@ describe('calculateSock', ()=>{
       heel: new Heel({startTurnStitches: 20, endTurnStitches: 4, bottomRows: 16, flapRows: 30, gussetStitchesAtWidest: 80, gussetRows: 20}),
     });
     expect(calculateSock(footE, gaugeRandom)).toEqual(expectedSockInstructions);
+  });
+
+  it('size 43', ()=>{
+    const expectedSockInstructions = new SockInstructions({
+      toe: new Toe({castOnStitches: 36, rows: 16}),
+      foot: new Foot({rows: 61, stitchesInRound: 68}),
+      roundLegStitches: 68,
+      heel: new Heel({
+        startTurnStitches: 24,
+        endTurnStitches: 4,
+        bottomRows: 20,
+        flapRows: 32,
+        gussetStitchesAtWidest: 88,
+        gussetRows: 20,
+      }),
+    });
+    expect(calculateSock(footB, gaugeSixPly)).toEqual(expectedSockInstructions);
   });
 
 });
